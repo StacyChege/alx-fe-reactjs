d4bb1e1f85f313ed29fba0ac59a1a597f11e8ca1
Stacy Chege <stacychege23@gmail.com>
2025-07-14 17:46:01 +0300

    Add WelcomeMessage component and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import WelcomeMessage from './components/WelcomeMessage.jsx'
+import WelcomeMessage from './components/WelcomeMessage.js'
 function App() {
   const [count, setCount] = useState(0)
 

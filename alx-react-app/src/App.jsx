32c84f5685ee4f9af8c6bc7990eca8337d859925
Stacy Chege <stacychege23@gmail.com>
2025-07-14 18:55:33 +0300

    Refactor App component by removing unused imports and state; add UserProfile component and create WelcomeMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-import WelcomeMessage from 'alx-fe-reactjs/alx-react-app/src/components/WelcomeMessage.jsx'
-import Header from 'alx-fe-reactjs/alx-react-app/src/components/Header.jsx'
-import MainContent from 'alx-fe-reactjs/alx-react-app/src/components/MainContent.jsx'
-import Footer from 'alx-fe-reactjs/alx-react-app/src/components/Footer.jsx'
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+// import Header from 'alx-fe-reactjs/alx-react-app/src/components/Header.jsx'
+// import MainContent from 'alx-fe-reactjs/alx-react-app/src/components/MainContent.jsx'
+// import Footer from 'alx-fe-reactjs/alx-react-app/src/components/Footer.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <Header />
-        <MainContent />
-        <Footer />
         <WelcomeMessage />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        {/* <Header /> */}
+        {/* <MainContent /> */}
+        {/* <Footer /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ alx-react-app/src/UserProfile.jsx
@@ -0,0 +1,9 @@
+function UserProfile() {
+    return (
+        <div>
+            <h2>user's name</h2>
+            <p>age</p>
+            <p>bio</p>
+        </div>
+    )
+}

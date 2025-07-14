d3215ff9ee91c708d985150a19e5dcaf76f65337
Stacy Chege <stacychege23@gmail.com>
2025-07-14 19:13:17 +0300

    Reintroduce UserProfile component with props for name, age, and bio; update App component to utilize the new UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,8 @@ import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
+import UserProfile from './components/UserProfile.jsx'; 
+
 function App() {
   return (
     <>
@@ -11,6 +13,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="John Doe" age={30} bio="Avid traveler and city explorer." />
     </>
   );
 }
--- alx-react-app/src/UserProfile.jsx
+++
@@ -1,9 +0,0 @@
-function UserProfile() {
-    return (
-        <div>
-            <h2>user's name</h2>
-            <p>age</p>
-            <p>bio</p>
-        </div>
-    )
-}

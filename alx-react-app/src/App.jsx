56dc6d7d4e25d8719d7eec7da219ace2a7abe03d
Stacy Chege <stacychege23@gmail.com>
2026-04-02 14:58:27 +0300

    User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,13 +2,16 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile name="Sarah" age={30} bio="A software developer from New York." />
       <Footer />
+
     </div>
   )
 }
